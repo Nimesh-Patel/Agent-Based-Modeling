6c55e5ff10b90ca5bdabdef4b4e56a1818050316
David Masad <david.masad@gmail.com>
2017-04-30 18:35:18 -0400

    Moving continuous visualization into main model
    Move the simple continuous space visualization from the Flockers
    example to the Mesa visualization package path.

--- examples/Flockers/flockers/server.py
+++ examples/Flockers/flockers/server.py
@@ -1,7 +1,9 @@
 from mesa.visualization.ModularVisualization import ModularServer
+from mesa.visualization.modules.SimpleContinuousModule import SimpleCanvas
+from mesa.visualization.UserParams import UserParam
 
 from .model import BoidModel
-from .SimpleContinuousModule import SimpleCanvas
+#from .SimpleContinuousModule import SimpleCanvas
 
 
 def boid_draw(agent):
@@ -11,6 +13,6 @@ def boid_draw(agent):
 boid_canvas = SimpleCanvas(boid_draw, 500, 500)
 
 model_params = dict(N=100, width=100, height=100, speed=5,
-                    vision=10, separation=2)
+                    vision=10, separation=UserParam(2, 1, 5, 1))
 server = ModularServer(BoidModel, [boid_canvas], "Boids", model_params)
 server.launch()
